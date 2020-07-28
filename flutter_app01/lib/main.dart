12e3c50b2bc37d15abff58701cf7dab39c529941
TonyInBeijing <1300565735@qq.com>
2020-07-28 16:07:16 +0800

    构造动态列表

--- flutter_app01/lib/main.dart
+++ flutter_app01/lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MyApp(items: new List<String>.generate(1000, (i) => "Item $i")));
 
 class MyApp extends StatelessWidget {
+  // 构造函数
+  final List<String> items;
+  MyApp({Key key, @required this.items}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Text Widget',
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            child: Text('Hello,JSpang', style: TextStyle(fontSize: 40)),
-            alignment: Alignment.center,
-            width: 500.0,
-            height: 400.0,
-            color: Colors.lightBlue,
-          ),
+      title: 'ListViewDemo',
+      home: new Scaffold(
+        body: new ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          },
         ),
       ),
     );
