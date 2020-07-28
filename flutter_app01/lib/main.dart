ed6c7a5b82cd83508242adee5e7bca1b4f56191a
TonyInBeijing <1300565735@qq.com>
2020-07-28 16:29:59 +0800

    网格列表

--- flutter_app01/lib/main.dart
+++ flutter_app01/lib/main.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MyApp(items: new List<String>.generate(1000, (i) => "Item $i")));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // 构造函数
-  final List<String> items;
-  MyApp({Key key, @required this.items}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ListViewDemo',
+    return new MaterialApp(
       home: new Scaffold(
-        body: new ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return new ListTile(
-              title: new Text('${items[index]}'),
-            );
-          },
+        appBar: new AppBar(
+          title: new Text('网格列表'),
+        ),
+        body: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 0.0,
+            crossAxisSpacing: 0.0,
+            childAspectRatio: 1.0,
+          ),
+          children: <Widget>[
+            new Image.network(
+              'https://t8.baidu.com/it/u=2247852322,986532796&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1596529150&t=ee13bc717e8f4fbfad470600811c4c55',
+              fit: BoxFit.cover,
+            ),
+            new Image.network(
+              'https://t8.baidu.com/it/u=2247852322,986532796&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1596529150&t=ee13bc717e8f4fbfad470600811c4c55',
+              fit: BoxFit.cover,
+            ),
+            new Image.network(
+              'https://t8.baidu.com/it/u=2247852322,986532796&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1596529150&t=ee13bc717e8f4fbfad470600811c4c55',
+              fit: BoxFit.cover,
+            ),
+            new Image.network(
+                'https://t8.baidu.com/it/u=2247852322,986532796&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1596529150&t=ee13bc717e8f4fbfad470600811c4c55',
+                fit: BoxFit.cover),
+            new Image.network(
+              'https://t8.baidu.com/it/u=2247852322,986532796&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1596529150&t=ee13bc717e8f4fbfad470600811c4c55',
+              fit: BoxFit.cover,
+            ),
+          ],
         ),
       ),
     );
